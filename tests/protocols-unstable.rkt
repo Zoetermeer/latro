6cd450859d5dc1f9649f8d0efa20c1af78df24a6
Zoetermeer <james.swaine@gmail.com>
2017-07-18 14:30:37 -0500

    Add protocol test suite to CI harness

--- tests/protocols-unstable.rkt
+++ tests/protocols-unstable.rkt
@@ -45,4 +45,39 @@
           }
         }
         '("False" "True" "False"))))
+
+  (test-case "it selects the right overload when operand types are unknown at compile time"
+    (parameterize ([use-core? #f])
+      (check-equal?
+        @interp-lines{
+          module Program {
+            protocol Show(a) {
+              show : a -> String
+            }
+
+            type Int = primtype(int)
+            type Char = primtype(char)
+            type String = Char[]
+            type Unit = primtype(unit)
+
+            imp Int : Show {
+              show(_) = "it's an int"
+            }
+
+            imp String : Show {
+              show(s) = s
+            }
+
+            println : a -> Unit when a : Show
+            println(v) = prim(println)(v)
+
+            printv(v) = println(show(v))
+
+            main(_) = {
+              printv(42)
+              printv("hello")
+            }
+          }
+        }
+        '("42"))))
 )
