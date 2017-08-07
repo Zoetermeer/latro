8dd2ef181e164fb8f24860099d4085304a33f9da
Zoetermeer <james.swaine@gmail.com>
2017-08-07 16:19:49 -0500

    Update unstable tests for #65

--- tests/protocols-unstable.rkt
+++ tests/protocols-unstable.rkt
@@ -4,42 +4,27 @@
   (require "common.rkt"
            rackunit)
 
-  (test-case "it selects the right overload when operand types are unknown at compile time"
+  (test-case "it rejects unspecified constraints on rigid type variables"
     (parameterize ([use-core? #f])
-      (check-equal?
-        @interp-lines{
+      (check-match
+        @interp-sexp{
           module Main {
-            type Bool = primtype(bool)
-            type Int = primtype(int)
+            type Char = primtype(char)
+            type String = Char[]
 
-            protocol Equatable(a) {
-              infixl (==) : a -> a -> Bool
+            protocol Show(a) {
+              show : a -> String
             }
 
-            imp Equatable(Int) {
-              infixl (==)(a, b) = prim(intEq)(a, b)
+            imp Show(Char) {
+              show(c) = [c]
             }
 
-            mod(n, d) = prim(intMod)(n, d)
+            showIt<a> : a -> String
+            showIt(v) = show(v)
 
-            infixl (++)([], bs)    = bs
-            infixl (++)(as, [])    = as
-            infixl (++)(a @"@" as, bs) = a @"@" (as ++ bs)
-
-            infixl (/)(a, b) = prim(intDiv)(a, b)
-
-            digits : Int -> Int[]
-            digits(0) = [0]
-            digits(n) = {
-              let m = mod(n, 10)
-              let n' = n / 10
-              if (n' == 0) [m] digits(n') ++ [m]
-            }
-
-            main(_) = {
-              prim(println)(digits(42))
-            }
+            main(_) = prim(println)(showIt('x'))
           }
         }
-        '("[4, 2]"))))
+        `(AtPos (SourcePos ,_ 16 ,_) (CompilerModule Typecheck) (CannotResolve Show a)))))
 )
--- tests/typechecker-unstable.rkt
+++ tests/typechecker-unstable.rkt
@@ -4,54 +4,100 @@
   (require "common.rkt"
            rackunit)
 
-  (test-case "partition"
+  #|
+  (test-case "it works"
+    (parameterize ([use-core? #f])
+      (check-match
+        @interp-sexp{
+          module Main {
+            type Int = primtype(int)
+            type Char = primtype(char)
+
+            foo<t> : Int -> t
+            foo(42) = 'c'
+
+            main(_) = prim(println)(foo(42))
+          }
+        }
+        `(AtPos (SourcePos ,_ 8 ,_) (CompilerModule Typecheck) (CantUnify (Var t) Char)))))
+
+  (test-case "it unifies user-specified concrete types with inferred type variables"
     (parameterize ([use-core? #f])
       (check-equal?
         @interp-lines{
-          module Main {
-            type Int = primtype(int)
-            type Bool = primtype(bool)
-            type Char = primtype(char)
-            type String = Char[]
+          module Main {    
+            type Unit = primtype(unit)
 
-            //infixl (+)(a, b) = prim(intAdd)(a, b)
-            //infixl (==)(a, b)  = prim(intEq)(a, b)
-
-            //length<a> : a[] -> Int
-            //length([])    = 0
-            //length(x @"@" xs) = 1 + length(xs)
-
-            foldr<a, b> : (a -> b -> b) -> b -> a[] -> b
-            foldr(f, b, x @"@" xs) = f(x, foldr(f, b, xs))
-            foldr(f, b, [])   = b
-
-
-            select<a> : (a -> Bool) -> a -> %(a[], a[]) -> %(a[], a[])
-            select(p, x, %(ts, fs)) =
-              cond {
-                p(x) -> %(x @"@" ts, fs)
-                _    -> %(ts, x @"@" fs)
-              }
-
-
-            partition<a> : (a -> Bool) -> a[] -> %(a[], a[])
-            partition(p, xs) = {
-              let selector = (fun(x, pr) = { select(p, x, pr) })
-              //foldr(fun(x, pr) = select(p, x, pr), %([], []), xs)
-              foldr(selector, %([], []), xs)
+            each<a> : (a -> Unit) -> a[] -> Unit
+            each(_, [])   = ()
+            each(f, x @"@" xs) = {
+              f(x)
+              each(f, xs)
             }
-
-            //isFiveChars : String -> Bool
-            //isFiveChars(str) = length(str) == 5
-            isHello("hello") = True
-            isHello(_) = False
-
+            
             main(_) = {
-              prim(println)(
-                partition(
-                  isHello, ["he", "hello", "world", "foo", "bar", "fubar"]))
+              each(fun(x) = prim(println)(x), [1, 2, 3]) 
             }
           }
         }
-        '("%([\"hello\", \"world\", \"fubar\"], [\"he\", \"foo\", \"bar\"])"))))
+      '("1" "2" "3"))))
+
+  (test-case "it allows named type declarations inside type modules"
+		(parameterize ([use-core? #f])
+			(check-match
+				@interp-lines{
+					module Root.Lst {
+						type Int = primtype(int)
+						import Root
+
+						type<a> | Cons(a, Lst<a>)
+										| Nil
+
+						tail<b> : Lst<b> -> Lst<b>
+						tail(Nil) = Nil()
+						tail(Cons(_, tl)) = tl
+
+						type IntList = Lst<Int>
+					}
+
+					module Main {
+						import Root.Lst
+
+						main(_) = {
+							ints : IntList
+							let ints = Cons(42, Cons(43, Nil()))
+							prim(println)(tail(ints))
+						}
+					}
+				}
+				'("Cons(43, Nil)"))))
+  |#
+
+  (test-case "it allows mutually recursive refs between adt's and aliases"
+		(parameterize ([use-core? #f])
+			(check-equal?
+				@interp-lines{
+					module Main {
+						type Int = primtype(int)
+
+						type Lst<b> = t<b>
+
+						type t <a> =
+							| Cons(a, Lst<a>)
+							| Nil
+
+						type IntList = Lst<Int>
+
+						tail<c> : Lst<c> -> Lst<c>
+						tail(Nil) = Nil()
+						tail(Cons(_, tl)) = tl
+
+						main(_) = {
+							ints : IntList
+							let ints = Cons(42, Cons(43, Nil()))
+							prim(println)(tail(ints))
+						}
+					}
+				}
+				'("Cons(43, Nil)"))))
 )
